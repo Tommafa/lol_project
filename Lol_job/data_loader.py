5653155f2bd0a87389caad1c0569e20c6028cc70
Tommafa <tommasolanni94@gmail.com>
2022-11-10 17:25:07 +0100

    adding precommit and new columns

--- Lol_job/data_loader.py
+++ Lol_job/data_loader.py
@@ -1,64 +1,70 @@
-import yaml
 from resources import base_utils as bu
-from resources.league_objects import *
 import pandas as pd
 import resources.riot_api_readers as riot_r
-import botocore
-import botocore.session
-#from aws_secretsmanager_caching import SecretCache, SecretCacheConfig
 from sqlalchemy import create_engine, schema
 import urllib
-import pyodbc
+
 
 def main(env_config):
-
-
     config_yaml_path = env_config["config_path"]
 
     if env_config["env"] == "development":
-        # if dev we can read the connection keys ( db and api) from a file, otherwise they are secrets
+        # if dev we can read the connection keys ( db and api) from a file,
+        # otherwise they are secrets
         header = env_config["header"]
         db_settings = env_config["db_settings"]
     else:
-        client = botocore.session.get_session().create_client('secretsmanager')
-        cache_config = SecretCacheConfig()
-        cache = SecretCache(config=cache_config, client=client)
-
-        header = cache.get_secret_string('header')
-        db_settings = cache.get_secret_string('db_settings')
+        pass
+        # client = botocore.session.get_session().
+        # create_client('secretsmanager')
+        # cache_config = SecretCacheConfig()
+        # cache = SecretCache(config=cache_config,
+        # client=client)
+        #
+        # header = cache.get_secret_string('header')
+        # db_settings = cache.get_secret_string('db_settings')
 
     # used to obtain the lists of tiers and divisions
     league_structure = bu.read_yaml(config_yaml_path)["leagues_structure"]
 
-
-
     # setup connection
-    quoted = urllib.parse.quote_plus(db_settings["connection_string"].format(db_settings["server"], db_settings["db_name"], db_settings["password"]))
-    engine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted))
-    # connection = pyodbc.connect(db_settings["connection_string"].format(db_settings["server"], db_settings["db_name"], db_settings["password"]))
+    quoted = urllib.parse.quote_plus(
+        db_settings["connection_string"].format(db_settings["server"],
+                                                db_settings["db_name"],
+                                                db_settings["password"]))
+    engine = create_engine('mssql+pyodbc:///?odbc_connect={}'
+                           .format(quoted), fast_executemany=True)
 
     if db_settings["schema"] not in engine.dialect.get_schema_names(engine):
         engine.execute(schema.CreateSchema(db_settings["schema"]))
 
     # verify schema and create it eventually
-    #bu.verify_schema(connection, db_settings)
-
+    # bu.verify_schema(connection, db_settings)
 
     print("schema creato")
-    # given a set of settings for LOL API structure and the header, produce the dict with keys that are going to be
+    # given a set of settings for LOL API structure and the header,
+    # produce the dict with keys that are going to be
     # our dataset's columns
-    summoners_dict = riot_r.load_list_of_summoners(league_structure["summoners_reader"], header, verbose=True)# header is used for api consumption
+    summoners_dict, mini_series_dict = riot_r.\
+        load_list_of_summoners(league_structure["summoners_reader"],
+                               header, verbose=True)
+    # header is used for api consumption
     print("dati scaricati")
+
+    # merge dicts in a single one
+    summoners_dict.update(mini_series_dict)
 
     # transform dict to pandas dataframe
     summoners_pd = pd.DataFrame.from_dict(summoners_dict)
+
+    print(summoners_pd.columns)
     summoners_pd.drop("miniSeries", axis=1, inplace=True)
     # write data to table
 
-
     summoners_pd.to_sql("summoners", schema=db_settings["schema"],
                         con=engine, if_exists="replace", index=False,
-                        index_label="summonerId")
+                        index_label="summonerId", method="multi",
+                        chunksize=100)
 
     print("dati scritti su tabella")
 
@@ -68,7 +74,10 @@ def main(env_config):
 if __name__ == "__main__":
 
     # read env file to understand where to find information
-    env_conf = bu.read_yaml("resources/env_config/env_config.yaml")
+    try:
+        env_conf = bu.read_yaml("resources/env_config.yaml")
 
-    # TODO: define connection settings based on env
-    main(env_conf)
+        main(env_conf)
+    except FileNotFoundError:
+        print("prepara un file di "
+              "configurazione come da istruzioni nel Readme")
--- Lol_job/resources/riot_api_readers.py
+++ Lol_job/resources/riot_api_readers.py
@@ -1,89 +1,87 @@
-from typing import List, Dict, Any
-
+from typing import List, Dict
 import requests
-import pydantic
-import urllib.request as urlreq
-import urllib.parse as urlp
-from resources.league_objects import *
-import json
+from resources.league_objects import LeagueEntryDTO, MiniSeriesDTO
 import time
 
 
-# TODO: change this function to organize it better
-def build_summoners_links_per_division(configurations_for_summoners_reader: dict, headers: dict,
-                                       verbose: bool = True) -> List[dict]:
+def build_summoners_links_per_division(
+        config_summoners_reader: dict,
+        headers: dict,
+        verbose: bool = True) -> List[dict]:
     """build list of tiers and divisions to load"""
 
     links_characteristics_list = []
     # define challenger dict
-    challenger = {"base_link": configurations_for_summoners_reader["base_path"],
-                  "queue_type": configurations_for_summoners_reader["queue_type"],
-                  "tier": configurations_for_summoners_reader["tiers"][0],
-                  "division": configurations_for_summoners_reader["divisions"][0],
-                  "page": 1,
-                  "headers": headers,
-                  "verbose": verbose
-
-                  }
+    challenger = \
+        dict(
+                base_link=config_summoners_reader["base_path"],
+                queue_type=config_summoners_reader["queue_type"],
+                tier=config_summoners_reader["tiers"][0],
+                division=config_summoners_reader["divisions"][0],
+                page=1, headers=headers, verbose=verbose)
 
     # define grandmaster dict
-    grandmaster = {"base_link": configurations_for_summoners_reader["base_path"],
-                   "queue_type": configurations_for_summoners_reader["queue_type"],
-                   "tier": configurations_for_summoners_reader["tiers"][1],
-                   "division": configurations_for_summoners_reader["divisions"][0],
-                   "page": 1,
-                   "headers": headers,
-                   "verbose": verbose
-
-                   }
+    grandmaster = \
+        dict(
+                base_link=config_summoners_reader["base_path"],
+                queue_type=config_summoners_reader["queue_type"],
+                tier=config_summoners_reader["tiers"][1],
+                division=config_summoners_reader["divisions"][0],
+                page=1, headers=headers, verbose=verbose)
 
     # define master dict
-    master = {"base_link": configurations_for_summoners_reader["base_path"],
-              "queue_type": configurations_for_summoners_reader["queue_type"],
-              "tier": configurations_for_summoners_reader["tiers"][2],
-              "division": configurations_for_summoners_reader["divisions"][0],
-              "page": 1,
-              "headers": headers,
-              "verbose": verbose
-
-              }
+    master = \
+        dict(
+            base_link=config_summoners_reader["base_path"],
+            queue_type=config_summoners_reader["queue_type"],
+            tier=config_summoners_reader["tiers"][2],
+            division=config_summoners_reader["divisions"][0],
+            page=1, headers=headers, verbose=verbose)
 
     links_characteristics_list.extend([challenger, grandmaster, master])
 
     # define dicts for all the other leagues
-    links_characteristics_list.extend([{"base_link": configurations_for_summoners_reader["base_path"],
-                                        "queue_type": configurations_for_summoners_reader["queue_type"],
-                                        "tier": tier,
-                                        "division": division,
-                                        "page": 1,
-                                        "headers": headers,
-                                        "verbose": verbose
-                                        }
-                                       for i, tier in enumerate(configurations_for_summoners_reader["tiers"][3:])
-                                       for j, division in enumerate(configurations_for_summoners_reader["divisions"])])
+    for i, tier in enumerate(config_summoners_reader["tiers"][3:]):
+        for j, division in \
+                enumerate(config_summoners_reader["divisions"]):
+            links_characteristics_list\
+                .extend([dict(
+                            base_link=config_summoners_reader["base_path"],
+                            queue_type=config_summoners_reader["queue_type"],
+                            tier=tier, division=division, page=1,
+                            headers=headers,
+                            verbose=verbose)])
 
     return links_characteristics_list
 
 
-def load_summoners_from_riot_api(base_link: str, queue_type: str, tier: str, division: str, page: int,
+def load_summoners_from_riot_api(base_link: str, queue_type: str, tier: str,
+                                 division: str, page: int,
                                  headers: dict,
-                                 verbose: bool = True) -> str:
+                                 verbose: bool = True) -> bytes:
     """load a list of summoners ordered by rank"""
 
     # fill link structure with input params
-    link_for_request = "{}{}/{}/{}?page={}".format(base_link, queue_type, tier, division, str(page))
+    link_for_request = "{}{}/{}/{}?page={}".format(base_link,
+                                                   queue_type,
+                                                   tier,
+                                                   division, str(page))
 
     # used to repeat the api call if we have code different from 200
     problems_at_previous_iteration = True
     iteration = 0
-    while problems_at_previous_iteration and iteration < 5:# repeat up to 5 times
+    while problems_at_previous_iteration and iteration < 5:
+        # repeat up to 5 times
         try:
 
-            response = requests.get(link_for_request, headers=headers)
+            response = requests.get(link_for_request,
+                                    headers=headers)
             status_code = response.status_code
             if verbose:
-                msg = "Everything went well!" if status_code == 200 else "There was an error when handling the request for the following link: {}.".format(
-                    link_for_request)
+                msg = "Everything went well!" if status_code == 200 \
+                    else "There was an error when handling the " \
+                         "request for the following link: {}." \
+                    .format(link_for_request)
 
                 print(msg)
             if status_code == 200:
@@ -92,29 +90,53 @@ def load_summoners_from_riot_api(base_link: str, queue_type: str, tier: str, div
             else:
                 if verbose:
                     print(response.content)
-                # usage limit is n calls every 2 minutes --> when I get an error I wait 2 minutes as it will reset
+                # usage limit is n calls every 2 minutes -->
+                # when I get an error I wait 2 minutes as it will reset
                 time.sleep(120)
-
+                iteration += 1
         except Exception as e:
             iteration += 1
-            print("Unable to get url {} due to {}.".format(link_for_request, e.__class__))
+            print("Unable to get url {} due to {}."
+                  .format(link_for_request, e.__class__))
 
 
-def load_list_of_summoners(configurations_for_summoners_reader: dict, header: dict, verbose: bool = True) -> Dict[str,list]:
+def load_list_of_summoners(configurations_for_summoners_reader: dict,
+                           header: dict, verbose: bool = True) \
+        -> tuple[Dict[str, list], Dict[str, list]]:
     """Retrieves a dataframe with values of each summoner"""
     # prepare dict for pandas df creation
     table_structure_summoners = {}
-    for key in LeagueEntryDTO.schema()["properties"].keys():
+    table_structure_mini_series = {}
+    for key in LeagueEntryDTO.schema()["properties"]:
         table_structure_summoners[key] = []
 
+    for key in MiniSeriesDTO.schema()["properties"]:
+        table_structure_mini_series[f"mini_series_{key}"] = []
 
         # create full list of leagues to load
-        links_per_division = build_summoners_links_per_division(configurations_for_summoners_reader, header, verbose)
+        links_per_division = \
+            build_summoners_links_per_division(
+                configurations_for_summoners_reader,
+                header, verbose)
     print("table structure created")
-    # use accessory list comprehension to: call the API, decode the bytes to string, create the dict to append to our
-    # initial pandas structure ( the LeagueEntryDTO is necessary as sometimes the miniSeries is absent..)
-    [table_structure_summoners[key].append(LeagueEntryDTO(**summoner).dict()[key]) for summoners in links_per_division
-     for summoner in eval(
-        load_summoners_from_riot_api(**summoners).decode("utf-8").replace("true", "True").replace("false", "False"))
-     for key in table_structure_summoners.keys()]
-    return table_structure_summoners
+    # for loop to: call the API, decode the bytes to string,
+    # create the dict to append to our
+    # initial pandas structure
+    # ( the LeagueEntryDTO is necessary as sometimes
+    # the miniSeries is absent..)
+    for summoners in links_per_division:
+        for summoner in \
+                eval(load_summoners_from_riot_api(**summoners)
+                     .decode("utf-8")
+                     .replace("true", "True")
+                     .replace("false", "False")):
+            league_entry = LeagueEntryDTO(**summoner).dict()
+            mini_series_entr = MiniSeriesDTO(**league_entry["miniSeries"])\
+                .dict()
+            for key in table_structure_summoners:
+                table_structure_summoners[key].append(league_entry[key])
+            for key in MiniSeriesDTO.schema()["properties"]:
+                table_structure_mini_series[f"mini_series_{key}"]\
+                    .append(mini_series_entr[key])
+
+    return table_structure_summoners, table_structure_mini_series
